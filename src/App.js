493bda6bee42c706170a3f7eeaba4d270dbf73bf
Prolific Blueprint <info@prolificblueprint.com>
2021-08-11 17:29:15 -0400

    latest with matcap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
-import './styles/App.scss';
+import "./styles/App.scss";
 import * as THREE from "three";
-import * as _ from 'lodash';
+import * as _ from "lodash";
 
-import { FiMenu } from 'react-icons/fi';
+import { FiMenu } from "react-icons/fi";
 
 // import gsap from "gsap";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // import { DragControls } from "three/examples/jsm/controls/DragControls";
 
-
-import { rubik_colors, color_opt_array } from "./cubes/colors";
-import Time from './components/time';
-import Controls from './components/controls';
+import {
+  shuffle,
+  isWinningCombination,
+  generateMasterCubes,
+  moveAvailableSqByDirection,
+} from "./helper";
+import { rubik_colors, color_opt_array, rubik_matcaps } from "./cubes/colors";
+import Time from "./components/time";
+import Controls from "./components/controls";
 
 // debuger
 // const gui = new dat.GUI({ closed: true });
@@ -21,7 +26,6 @@ import Controls from './components/controls';
 let camera, scene, renderer, appEl;
 let controls;
 let cubes = [];
-let masterCubes = [];
 let gamePieces;
 let masterGameMap = new Map();
 const boardGameMap = new Map();
@@ -42,10 +46,41 @@ boardGameMap.set(0, boardGameMap_row1);
 boardGameMap.set(1, boardGameMap_row2);
 boardGameMap.set(2, boardGameMap_row3);
 
-
 let cubeSize = 1;
 const gridCount = 5;
-const masterGridCount = 3;
+
+const fontLoader = new THREE.FontLoader();
+const textureLoader = new THREE.TextureLoader();
+
+const matcapTexture = textureLoader.load('/matcaps/bluesteel256px.png');
+
+fontLoader.load("/fonts/Avenir_Roman_Bold.json", (font) => {
+  console.log("font loaded.. .");
+  const bevelSize = 0.02;
+  const bevelThickness = 0.03;
+  const text_geometry = new THREE.TextBufferGeometry("3 2 1", {
+    font,
+    color: 0xffa500,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 7,
+    bevelEnabled: true,
+    bevelThickness: bevelThickness,
+    bevelSize: bevelSize,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+
+  text_geometry.center();
+
+  const text_material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+  const text = new THREE.Mesh(text_geometry, text_material);
+  text.position.z = 2;
+  // scene.add(text);
+});
+
+
+
 
 class App extends Component {
   constructor() {
@@ -57,7 +92,7 @@ class App extends Component {
       gamneCubes: [],
       moveCount: 0,
       isGameComplete: false,
-      isMenuOpen: false
+      isMenuOpen: false,
     };
   }
 
@@ -77,7 +112,6 @@ class App extends Component {
     renderer.render(scene, camera);
   };
 
-
   bindResize = () => {
     window.addEventListener("resize", (e) => {
       camera.aspect = window.innerWidth / window.innerHeight;
@@ -92,36 +126,36 @@ class App extends Component {
       const dir = e.key;
       this.moveGamePieceByDirection(dir);
     });
-  }
+  };
 
   directionalButtonPress = (e) => {
     e.preventDefault();
     const dir = e.target.getAttribute("data-direction");
     this.moveGamePieceByDirection(dir);
-  }
+  };
 
   generateMasterCubes = () => {
-    const cubeArr = generateMasterCubes(masterGameMap)
+    const cubeArr = generateMasterCubes(masterGameMap);
     this.setState({ masterCubeArr: cubeArr });
   };
-  
+
   startClock = () => {
     if (!this.state.isClockRunning) {
       this.setState({ isClockRunning: true });
     }
-  }
+  };
 
   updateParentClockOnComplete = (time) => {
     this.setState({ clock: time, isClockRunning: false });
-  }
+  };
 
   moveGamePieceByDirection = (arrowDirection) => {
     let isAnimating = false;
 
     switch (arrowDirection) {
-      case 'ArrowLeft':
+      case "ArrowLeft":
         break;
-      case 'ArrowUp':
+      case "ArrowUp":
         this.setState({ isAnimatingUp: true });
         break;
       default:
@@ -131,31 +165,32 @@ class App extends Component {
     if (!this.debouncedFn) {
       this.debouncedFn = _.debounce((key) => {
         switch (key) {
-          case 'ArrowLeft': // left
+          case "ArrowLeft": // left
             moveAvailableSqByDirection(cubes, "L");
             this.setState({ moveCount: this.state.moveCount + 1 });
             isAnimating = false;
             break;
 
-          case 'ArrowUp': // up
+          case "ArrowUp": // up
             this.setState({ moveCount: this.state.moveCount + 1 });
             moveAvailableSqByDirection(cubes, "T");
             isAnimating = false;
             break;
 
-          case 'ArrowRight': // right
+          case "ArrowRight": // right
             moveAvailableSqByDirection(cubes, "R");
             this.setState({ moveCount: this.state.moveCount + 1 });
             isAnimating = false;
             break;
 
-          case 'ArrowDown': // down
+          case "ArrowDown": // down
             moveAvailableSqByDirection(cubes, "B");
             this.setState({ moveCount: this.state.moveCount + 1 });
             isAnimating = false;
             break;
 
-          default: return; // exit this handler
+          default:
+            return; // exit this handler
         }
       }, 110);
     }
@@ -166,7 +201,7 @@ class App extends Component {
     }
 
     this.startClock();
-  }  
+  };
 
   checkCombinationMatches = () => {
     gamePieces.children.forEach((p) => {
@@ -180,17 +215,19 @@ class App extends Component {
         boardGameMap.get(x + 1).set(y + 1, p.userData.color);
       }
       return null;
-    })
+    });
 
     if (isWinningCombination(boardGameMap, masterGameMap)) {
       this.setState({ isGameComplete: true });
-      console.log(" You Win! moves : " + this.state.moveCount + " / Time : ", this.state.clock);
+      console.log(
+        " You Win! moves : " + this.state.moveCount + " / Time : ",
+        this.state.clock
+      );
     }
-  }
+  };
 
   generateCubes = () => {
     gamePieces = new THREE.Group();
-
 
     const doubleCubeSize = cubeSize * 2;
     let count = 0;
@@ -201,19 +238,17 @@ class App extends Component {
     for (i = 0; i < gridCount; i++) {
       for (j = 0; j < gridCount; j++) {
 
-        let material = new THREE.MeshBasicMaterial({
-          color: rubik_colors[randomColors[count]],
-        });
+        // TODO matcap
+        let matcap_material = new THREE.MeshMatcapMaterial({ matcap: rubik_matcaps[randomColors[count]], });
+        // let material = new THREE.MeshBasicMaterial({
+        //   color: rubik_colors[randomColors[count]],
+        // });
 
         let cube_geometry;
         let xPos = -1 * i + cubeSize * 2;
         let yPos = -1 * j + cubeSize * 2;
 
-        cube_geometry = new THREE.BoxGeometry(
-          cubeSize,
-          cubeSize,
-          0.1
-        );
+        cube_geometry = new THREE.BoxGeometry(cubeSize, cubeSize, 0.1);
 
         // let isX = xPos === 1 || xPos === 0 || xPos === -1;
         // let isY = yPos === 1 || yPos === 0 || yPos === -1;
@@ -232,8 +267,7 @@ class App extends Component {
         //   );
         // }
 
-
-        const cube = new THREE.Mesh(cube_geometry, material);
+        const cube = new THREE.Mesh(cube_geometry, matcap_material);
         cube.position.x = xPos;
         cube.position.y = yPos;
         cube.userData.id = `${i}${j}`;
@@ -248,10 +282,18 @@ class App extends Component {
           if ($this.userData.intersects.length > 0) {
             let dataIntersects = $this.userData.intersects;
 
-            const TopLimit = dataIntersects.find((obj) => Object.keys(obj).includes("T"));
-            const RightLimit = dataIntersects.find((obj) => Object.keys(obj).includes("R"));
-            const BottomLimit = dataIntersects.find((obj) => Object.keys(obj).includes("B"));
-            const LeftLimit = dataIntersects.find((obj) => Object.keys(obj).includes("L"));
+            const TopLimit = dataIntersects.find((obj) =>
+              Object.keys(obj).includes("T")
+            );
+            const RightLimit = dataIntersects.find((obj) =>
+              Object.keys(obj).includes("R")
+            );
+            const BottomLimit = dataIntersects.find((obj) =>
+              Object.keys(obj).includes("B")
+            );
+            const LeftLimit = dataIntersects.find((obj) =>
+              Object.keys(obj).includes("L")
+            );
 
             const xMin = LeftLimit ? LeftLimit.L : -1 * doubleCubeSize;
             const yMin = BottomLimit ? BottomLimit.B : -1 * doubleCubeSize;
@@ -261,8 +303,16 @@ class App extends Component {
             cube_limit.max = new THREE.Vector3(xMax, yMax, 0);
             cube_limit.min = new THREE.Vector3(xMin, yMin, 0);
           } else {
-            cube_limit.min = new THREE.Vector3(-1 * doubleCubeSize, -1 * doubleCubeSize, 0);
-            cube_limit.max = new THREE.Vector3(doubleCubeSize, doubleCubeSize, 0);
+            cube_limit.min = new THREE.Vector3(
+              -1 * doubleCubeSize,
+              -1 * doubleCubeSize,
+              0
+            );
+            cube_limit.max = new THREE.Vector3(
+              doubleCubeSize,
+              doubleCubeSize,
+              0
+            );
           }
 
           //
@@ -276,7 +326,7 @@ class App extends Component {
         cube.userData.checkCombinationMatches = () => {
           // console.log('Calling map on : ', this);
           this.checkCombinationMatches();
-        }
+        };
 
         cubes.push(cube);
 
@@ -324,9 +374,13 @@ class App extends Component {
      */
   };
 
-
   init = () => {
-    camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.01, 100);
+    camera = new THREE.PerspectiveCamera(
+      90,
+      window.innerWidth / window.innerHeight,
+      0.01,
+      100
+    );
     camera.position.z = 7;
     scene = new THREE.Scene();
 
@@ -342,20 +396,45 @@ class App extends Component {
     this.generateCubes();
     this.generateMasterCubes();
     appEl.appendChild(renderer.domElement);
+
+    const ambientLight = new THREE.AmbientLight('0xFFFFFF', 0);
+    scene.add(ambientLight);
+
+    // const directionLight = new THREE.DirectionalLight('0x0000ff', 1);
+    // directionLight.position.set(1, 0, 2)
+    // scene.add(directionLight);
+
+    const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3)
+scene.add(hemisphereLight);
+
+
+const pointLight = new THREE.PointLight(0xff9000, 0.5)
+pointLight.position.set(1, - 0.5, 1);
+
+scene.add(pointLight);
+
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1)
+rectAreaLight.position.set(-1.5, 0, 1)
+rectAreaLight.lookAt(new THREE.Vector3())
+
+scene.add(rectAreaLight)
+
   };
 
-
   toggleMenu = () => {
-    this.setState({ isMenuOpen : !this.state.isMenuOpen })
-  }
-  
+    this.setState({ isMenuOpen: !this.state.isMenuOpen });
+  };
 
   render() {
     return (
       <>
         <div className="header">
           <span className="title">Rubix</span>
-          <Time isClockRunning={this.state.isClockRunning} isGameOver={this.state.isGameComplete} onGameComplete={this.updateParentClockOnComplete} />
+          <Time
+            isClockRunning={this.state.isClockRunning}
+            isGameOver={this.state.isGameComplete}
+            onGameComplete={this.updateParentClockOnComplete}
+          />
           <span className="menu" onClick={this.toggleMenu}>
             <FiMenu />
           </span>
@@ -369,9 +448,11 @@ class App extends Component {
         <Controls onMoveGamePieceByDirection={this.moveGamePieceByDirection} />
 
         <div className="masterGrid">
-          {this.state.masterCubeArr ? this.state.masterCubeArr.map((color, i) => {
-            return <div key={i} className={color}></div>
-          }) : 'Loading.. .'}
+          {this.state.masterCubeArr
+            ? this.state.masterCubeArr.map((color, i) => {
+                return <div key={i} className={color}></div>;
+              })
+            : "Loading.. ."}
         </div>
 
         <div className="webgl"></div>
--- src/cubes/colors.js
+++ src/cubes/colors.js
@@ -1,6 +1,15 @@
 // const material = new THREE.MeshMatcapMaterial()
 // material.matcap = matcapTexture
 // import all matcap textures here
+const THREE = require("three");
+const textureLoader = new THREE.TextureLoader();
+
+const matcapYellow = textureLoader.load('/matcaps/rubix_yellow_glossy.png');
+const matcapRed = textureLoader.load('/matcaps/rubix_red_glossy.png');
+const matcapBlue = textureLoader.load('/matcaps/rubix_blue.png');
+const matcapGreen = textureLoader.load('/matcaps/rubix_green.png');
+const matcapOrange = textureLoader.load('/matcaps/rubix_orange.png');
+const matcapWhite = textureLoader.load('/matcaps/rubix_white.png');
 
 const rubik_colors_hex = {
     blue: 0x0000ff,
@@ -10,6 +19,15 @@ const rubik_colors_hex = {
     green: 0x008000,
     red: 0xff0000,
 };
+
+const rubik_matcaps = {
+    blue: matcapBlue,
+    white: matcapWhite,
+    yellow: matcapYellow,
+    orange: matcapOrange,
+    green: matcapGreen,
+    red: matcapRed,
+}
 
 const color_opt_array = [
     "blue",
@@ -39,4 +57,5 @@ const color_opt_array = [
 ];
 
 module.exports.rubik_colors = rubik_colors_hex;
+module.exports.rubik_matcaps = rubik_matcaps;
 module.exports.color_opt_array = color_opt_array;
